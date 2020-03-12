a336ec8d27213c6c63505ac8df8d7e23f47c2637
Dharmin2 <60351663+Dharmin2@users.noreply.github.com>
2020-03-12 19:17:47 -0400

    Add files via upload

---
+++ Lab3Question5.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.*;
+
+public class Lab3Question5{
+    
+    public static void main(String[]args){
+        System.out.println("Enter birthday in yyyy-mm-dd");
+        System.out.println("Remember to add 0 in front mm 09 for september");
+        Scanner z=new Scanner(System.in);
+        String age= z.nextLine();
+        
+        LocalDate x=LocalDate.parse(age);
+        System.out.println("You are " + day(x));
+    }
+    public static int  day(LocalDate x){
+        LocalDate date=LocalDate.now();
+        return Period.between(x, date).getYears();
+    }
+}
