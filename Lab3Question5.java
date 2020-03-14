278d0aec3f70584792042cc6292696ec38846714
Dharmin2 <60351663+Dharmin2@users.noreply.github.com>
2020-03-14 15:58:47 -0400

    Add files via upload

--- Lab3Question5.java
+++ Lab3Question5.java
@@ -2,13 +2,13 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.*;
 import java.util.Calendar;
-import java.time.temporal.ChronoUnit;
 
-public class Main{
+public class Lab3Question5{
     
     public static int today(LocalDate x){
         LocalDate date=LocalDate.now();
         return Period.between(x,date).getYears();
+    }
     public static void main(String[]args){
         System.out.println("Enter birthday in yyyy-mm-dd");
         System.out.println("Remember to add 0 in front mm 09 for september");
@@ -26,7 +26,8 @@ public class Main{
                 nextBday=nextBday.plusYears(1);
             }
             Period p=Period.between(today,nextBday);
-            long pe = ChronoUnit.DAYS.between(today, nextBday);
-            System.out.println("Your birthday is in "+p.getMonths()+"months and "+p.getDays()+"days");
+            long pe=Period.between(today,nextBday).getMonths();
+            long pe2=Period.between(today,nextBday).getDays();
+            System.out.println("Your birthday is in "+p.getMonths()+" months and "+p.getDays()+" days");
         }
 }
